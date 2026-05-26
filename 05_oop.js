1646459007bee033ae4861e28b12cb0135bec44e
pjmoo <jung17min66@gmail.com>
2026-05-26 14:43:47 +0900

    05_oop.js

--- 05_oop.js
+++ 05_oop.js
@@ -21,3 +21,29 @@ const o1 = {
 };
 console.log(o1);
 o1.myFun();
+
+// 프로토타입 생성자
+function Student(name, major) {
+  // 외부에서 받을 매개변수
+  this.name = name;
+  this.major = major;
+  // ...
+  this.country = "한국";
+  this.hello = () => console.log("안녕");
+}
+
+const s1 = Student("Jane", "철학");
+const s2 = new Student("Jane", "철학"); // new 유무
+const s3 = new Student("Jane", "철학");
+// 기본/원시 타입 => 별도로 만들어주지 않아도 바로 쓸 수 있는 타입 목록.
+// 참조 타입 : 내가 생성자를 통해서 새롭게 만드는 객체별 구분해주는 타입 <- 프로토타입, 클래스
+// console.log("s1", s1);
+console.log("s2", s2);
+console.log("s3", s3);
+Student.prototype.bye = function () {
+  console.log("Bye");
+};
+console.log("s3.hello === s2.hello", s3.hello === s2.hello);
+console.log("s3.bye === s2.bye", s3.bye === s2.bye);
+
+// -> Class.
